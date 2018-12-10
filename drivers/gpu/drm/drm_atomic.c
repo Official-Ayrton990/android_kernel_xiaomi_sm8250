086fee671b8f7159413140c7a1d12cd114c0af77
Sultan Alsawaf <sultan@kerneltoast.com>
2018-12-09 17:45:01 -0800

    drm/atomic: Boost DDR when committing a new frame
    In order to reduce jank, a DDR boosts whenever a new frame
    is ready to be rendered to the display. The boosts should be sufficient
    enough to render 60 FPS without any dropped frames when there is no
    significant external source of load.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -33,6 +33,7 @@
 #include <linux/pm_qos.h>
 #include <drm/drm_writeback.h>
 #include <linux/sync_file.h>
+#include <linux/devfreq_boost.h>
 
 #include "drm_crtc_internal.h"
 #include "drm_internal.h"
@@ -2576,6 +2577,11 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 	if ((arg->flags & DRM_MODE_ATOMIC_TEST_ONLY) &&
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
+
+	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
+		devfreq_boost_kick(DEVFREQ_MSM_CPU_LLCCBW);
+		devfreq_boost_kick(DEVFREQ_MSM_LLCCBW_DDR);
+	}
 
 	drm_modeset_acquire_init(&ctx, DRM_MODESET_ACQUIRE_INTERRUPTIBLE);
 
