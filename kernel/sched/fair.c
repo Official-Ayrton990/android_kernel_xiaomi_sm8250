42a2b0637c2eabae0ca489f3fc6d03bbee657f3b
Zachariah Kennedy <zkennedy87@gmail.com>
2019-11-20 19:23:46 +0000

    sched/fair: split sched_boost from walt dependant code
    This allows us to leverage CAF's powerhal while !CONFIG_SCHED_WALT.
    
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>
    [alk3pInjection: Forward port to k4.19]
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    Change-Id: Id77d9fcebb954bd4cdf46fecaeb9eba311893d1e

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -170,13 +170,12 @@ unsigned int sched_capacity_margin_up[NR_CPUS] = {
 unsigned int sched_capacity_margin_down[NR_CPUS] = {
 			[0 ... NR_CPUS-1] = 1205}; /* ~15% margin */
 
-#ifdef CONFIG_SCHED_WALT
 /* 1ms default for 20ms window size scaled to 1024 */
 unsigned int sysctl_sched_min_task_util_for_boost = 51;
 /* 0.68ms default for 20ms window size scaled to 1024 */
 unsigned int sysctl_sched_min_task_util_for_colocation = 35;
 __read_mostly unsigned int sysctl_sched_prefer_spread;
-#endif
+
 unsigned int sched_small_task_threshold = 102;
 __read_mostly unsigned int sysctl_sched_force_lb_enable = 1;
 
@@ -11071,7 +11070,9 @@ no_move:
 				busiest->active_balance = 1;
 				busiest->push_cpu = this_cpu;
 				active_balance = 1;
+#ifdef CONFIG_SCHED_WALT				
 				mark_reserved(this_cpu);
+#endif
 			}
 			raw_spin_unlock_irqrestore(&busiest->lock, flags);
 
