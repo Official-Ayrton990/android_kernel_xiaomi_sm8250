ac0e8fb34afaf8a4b2e7a9b7894ffb7167b5ee4c
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2020-11-28 18:30:06 -0600

    proc: Kill facebook crap background
    Signed-off-by: Official-Ayrton990 <15030201@itcelaya.edu.mx>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -1135,7 +1135,8 @@ err_unlock:
 	put_task_struct(task);
 	/* These apps burn through CPU in the background. Don't let them. */
 	if (!err && oom_adj >= 700) {
-		if (!strcmp(task_comm, "id.GoogleCamera")) {
+                if (!strcmp(task_comm, "id.GoogleCamera") ||
+                    !strcmp(task_comm, "facebook.katana")) {
 			struct task_kill_info *kinfo;
 
 			kinfo = kmalloc(sizeof(*kinfo), GFP_KERNEL);
