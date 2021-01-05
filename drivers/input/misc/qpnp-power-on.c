216c6ecbfa00382cb081b590b0e3c059b8980403
alk3pInjection <webmaster@raspii.tech>
2021-01-05 14:45:51 +0800

    drivers: mtd: Merge Xiaomi changes [Ayrton]: Ported to SMxx50.0
    Co-authored-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- drivers/input/misc/qpnp-power-on.c
+++ drivers/input/misc/qpnp-power-on.c
@@ -981,6 +981,31 @@ static int qpnp_pon_input_dispatch(struct qpnp_pon *pon, u32 pon_type)
 	return 0;
 }
 
+static int longpress_kthread(void *_pon)
+{
+#ifdef CONFIG_MTD_BLOCK2MTD
+	struct qpnp_pon *pon = _pon;
+	ktime_t time_to_S2, time_S2;
+	struct sched_param param = {.sched_priority = MAX_RT_PRIO-1};
+
+	sched_setscheduler(current, SCHED_FIFO, &param);
+	dev_err(pon->dev, "Long press :Start to run longpress_kthread ");
+
+	ufs_enter_h8_disable(g_shost);
+	long_press();
+
+	time_S2 = pon->pon_cfg->s2_timer;
+	time_to_S2 = time_S2 - ktime_ms_delta(ktime_get(), pon->time_kpdpwr_bark);
+
+	if (time_to_S2 > 0)
+		mdelay(time_to_S2);
+
+	machine_restart(NULL);
+#endif
+
+	return 0;
+}
+
 static irqreturn_t qpnp_kpdpwr_irq(int irq, void *_pon)
 {
 	int rc;
