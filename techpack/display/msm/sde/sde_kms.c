0d4c84ddf7e1751a6d34835f162c8c523eabec41
alk3pInjection <webmaster@raspii.tech>
2021-01-07 14:05:34 +0800

    techpack: display: Merge Xiaomi changes
    [Ayrton]: Ported to SMxx50.0
    
    Co-authored-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- techpack/display/msm/sde/sde_kms.c
+++ techpack/display/msm/sde/sde_kms.c
@@ -1045,6 +1045,11 @@ static void sde_kms_commit(struct msm_kms *kms,
 		}
 	}
 
+/*
+	for_each_old_crtc_in_state(old_state, crtc, old_crtc_state, i) {
+		sde_crtc_fod_ui_ready(crtc, old_crtc_state);
+	}
+*/
 	SDE_ATRACE_END("sde_kms_commit");
 }
 
@@ -1195,6 +1200,8 @@ static void sde_kms_complete_commit(struct msm_kms *kms,
 			pr_err("Connector Post kickoff failed rc=%d\n",
 					 rc);
 		}
+
+		sde_connector_fod_notify(connector);
 	}
 
 	_sde_kms_drm_check_dpms(old_state, DRM_PANEL_EVENT_BLANK);
@@ -1259,7 +1266,7 @@ static void sde_kms_wait_for_commit_done(struct msm_kms *kms,
 		sde_crtc_complete_flip(crtc, NULL);
 	}
 
-	SDE_ATRACE_END("sde_ksm_wait_for_commit_done");
+	SDE_ATRACE_END("sde_kms_wait_for_commit_done");
 }
 
 static void sde_kms_prepare_fence(struct msm_kms *kms,
