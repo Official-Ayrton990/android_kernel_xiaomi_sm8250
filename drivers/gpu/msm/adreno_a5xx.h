e91aa7f6c76317cefcd6b55ad2ff6dd644974b7b
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2021-02-04 00:18:57 -0600

    Revert "msm: kgsl: Mark the scratch buffer as privileged"
    This reverts commit 21dc7da573b1282e5471e1b14ceb1130676ee2d0.
    
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- drivers/gpu/msm/adreno_a5xx.h
+++ drivers/gpu/msm/adreno_a5xx.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Copyright (c) 2015-2017,2019-2020 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2015-2017,2019 The Linux Foundation. All rights reserved.
  */
 
 #ifndef _ADRENO_A5XX_H_
