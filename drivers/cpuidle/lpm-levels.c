2c7d5d90a39b50d31ed50e55f6c937a84c42e520
LibXZR <xzr467706992@163.com>
2021-02-05 16:27:12 +0800

    lpm-levels: Don't expose sleep_disabled
    * Don't allow userspace do anything with it
    
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -94,9 +94,6 @@ static void cluster_prepare(struct lpm_cluster *cluster,
 static bool print_parsed_dt;
 module_param_named(print_parsed_dt, print_parsed_dt, bool, 0664);
 
-static bool sleep_disabled;
-module_param_named(sleep_disabled, sleep_disabled, bool, 0664);
-
 /**
  * msm_cpuidle_get_deep_idle_latency - Get deep idle latency value
  *
@@ -595,9 +592,6 @@ static inline bool lpm_disallowed(s64 sleep_us, int cpu, struct lpm_cpu *pm_cpu)
 
 	if (cpu_isolated(cpu))
 		goto out;
-
-	if (sleep_disabled)
-		return true;
 
 	bias_time = sched_lpm_disallowed_time(cpu);
 	if (bias_time) {
