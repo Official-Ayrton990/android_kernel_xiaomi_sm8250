d3e343f22bbce340ab7f0b7b2ce034df96399733
Adam W. Willis <return.of.octobot@gmail.com>
2021-05-01 22:24:13 -0500

    drm_atomic: Boost DDR bus when sched_boost is triggered
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -34,6 +34,7 @@
 #include <drm/drm_writeback.h>
 #include <linux/sync_file.h>
 #include <linux/devfreq_boost.h>
+#include <linux/sched/sysctl.h>
 
 #include "drm_crtc_internal.h"
 #include "drm_internal.h"
@@ -2578,7 +2579,7 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
 
-	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
+	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY) && sysctl_sched_boost) {
 		devfreq_boost_kick(DEVFREQ_MSM_LLCCBW_DDR);
 	}
 
