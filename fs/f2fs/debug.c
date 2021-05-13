0c6e9b1a7015922acd63d900d5775529b1dc6628
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2021-05-13 14:41:47 -0500

    fs: fix unterminated conditional directive
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- fs/f2fs/debug.c
+++ fs/f2fs/debug.c
@@ -58,7 +58,7 @@ void f2fs_update_sit_info(struct f2fs_sb_info *sbi)
 	else
 		si->avg_vblocks = 0;
 }
-#ifdef CONFIG_DEBUG_FS
+
 static void update_general_status(struct f2fs_sb_info *sbi)
 {
 	struct f2fs_stat_info *si = F2FS_STAT(sbi);
