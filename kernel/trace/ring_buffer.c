4336b0544a7d1c2ed8755d660449d730a216777c
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2021-05-18 22:51:22 -0500

    kernel: trace: remove a duplicated mutex_lock
    * Yes XiaoMi duplications are shit
    
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- kernel/trace/ring_buffer.c
+++ kernel/trace/ring_buffer.c
@@ -4396,7 +4396,6 @@ void ring_buffer_reset_cpu(struct ring_buffer *buffer, int cpu)
 	/* prevent another thread from changing buffer sizes */
 	mutex_lock(&buffer->mutex);
 
-	mutex_lock(&buffer->mutex);
 	atomic_inc(&buffer->resize_disabled);
 	atomic_inc(&cpu_buffer->record_disabled);
 
