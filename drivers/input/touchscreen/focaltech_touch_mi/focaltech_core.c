4b7cd65ad6c8e33b8bda52d19c0f2a4926ecc4bf
spakkkk <spakdroid@gmail.com>
2021-05-19 19:04:19 -0500

    input: focaltouch: Affine IRQ to the prime CPU cluster
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- drivers/input/touchscreen/focaltech_touch_mi/focaltech_core.c
+++ drivers/input/touchscreen/focaltech_touch_mi/focaltech_core.c
@@ -992,7 +992,7 @@ static int fts_irq_registration(struct fts_ts_data *ts_data)
 		pdata->irq_gpio_flags = IRQF_TRIGGER_FALLING;
 	FTS_INFO("irq flag:%x", pdata->irq_gpio_flags);
 	ret =
-		request_threaded_irq(ts_data->irq, NULL, fts_ts_interrupt, pdata->irq_gpio_flags | IRQF_ONESHOT,
+		request_threaded_irq(ts_data->irq, NULL, fts_ts_interrupt, pdata->irq_gpio_flags | IRQF_ONESHOT | IRQF_PRIME_AFFINE,
 				 ts_data->client->name, ts_data);
 
 	return ret;
