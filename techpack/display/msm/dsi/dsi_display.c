77f4847218ddc9dbfe566bbba3e9a870375ff004
Art_Chen <Chenxy0201@qq.com>
2021-06-05 07:05:11 +0530

    drm: add dimlayer_bl sysfs node
    [Ayrton: fixed an incompatible function pointer type initializing]
    
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- techpack/display/msm/dsi/dsi_display.c
+++ techpack/display/msm/dsi/dsi_display.c
@@ -197,6 +197,7 @@ void dsi_rect_intersect(const struct dsi_rect *r1,
 	}
 }
 
+bool is_dimlayer_bl_enable;
 int dsi_display_set_backlight(struct drm_connector *connector,
 		void *display, u32 bl_lvl)
 {
@@ -5200,6 +5201,25 @@ error:
 	return ret == 0 ? count : ret;
 }
 
+
+static ssize_t sysfs_dimlayer_bl_read(struct device *dev,
+	struct device_attribute *attr, char *buf)
+{
+    return snprintf(buf, PAGE_SIZE, "%d\n", is_dimlayer_bl_enable);
+}
+
+static ssize_t sysfs_dimlayer_bl_write(struct device *dev,
+	struct device_attribute *attr, const char *buf, size_t count)
+{
+    int enabled = 0;
+    sscanf(buf, "%d", &enabled);
+    is_dimlayer_bl_enable = enabled > 0;
+    return count;
+}
+
+static DEVICE_ATTR(dimlayer_bl, 0664,
+			sysfs_dimlayer_bl_read,
+			sysfs_dimlayer_bl_write);
 static DEVICE_ATTR(fod_ui, 0444,
 			sysfs_fod_ui_read,
 			NULL);
@@ -5211,6 +5231,7 @@ static DEVICE_ATTR(hbm, 0644,
 static struct attribute *display_fs_attrs[] = {
 	&dev_attr_fod_ui.attr,
 	&dev_attr_hbm.attr,
+	&dev_attr_dimlayer_bl.attr,
 	NULL,
 };
 static struct attribute_group display_fs_attrs_group = {
