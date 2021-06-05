7c56111467fe1f25ad5355f640c4cbd0b0e6a5d6
Danny Baumann <dannybaumann@web.de>
2021-06-05 06:00:47 +0530

    techpack: display/msm: Add support for high brightness mode (HBM)
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -227,6 +227,8 @@ struct dsi_panel {
 	int power_mode;
 	enum dsi_panel_physical_type panel_type;
 
+	int hbm_mode;
+
 	struct brightness_alpha_pair *fod_dim_lut;
 	uint32_t fod_dim_lut_count;
 };
@@ -368,5 +370,7 @@ void dsi_panel_dealloc_cmd_packets(struct dsi_panel_cmd_set *set);
 int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status);
 
 uint32_t dsi_panel_get_fod_dim_alpha(struct dsi_panel *panel);
+
+int dsi_panel_apply_hbm_mode(struct dsi_panel *panel);
 
 #endif /* _DSI_PANEL_H_ */
