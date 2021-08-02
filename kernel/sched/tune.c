1a3283a772cfa787cf987450344ef50d2ec98123
LibXZR <xzr467706992@163.com>
2021-08-03 03:47:21 +0530

    kernel: sched: tune: hardcode 1 to foreground tasks
    * Is something critical that people can touch and feel. Tasks inside foreground
    group is mostly highly related. Bind them and let those tasks migrate together.
    
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -814,7 +814,7 @@ static void write_default_values(struct cgroup_subsys_state *css)
 	static struct st_data st_targets[] = {
 		{ "audio-app",	0, 0},
 		{ "background",	0, 0},
-		{ "foreground",	0, 0},
+		{ "foreground",	1, 1},
 		{ "rt",		0, 0},
 		{ "top-app",	10, 1},
 	};
