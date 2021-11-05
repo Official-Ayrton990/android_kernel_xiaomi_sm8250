7ec5720e515b6d10a62a63a88e7aa6108def68c8
EcrosoftXiao <xy19990903@gmail.com>
2021-11-05 11:26:12 +0800

    techpack: display: Make dimlayer exposure switchable
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -231,6 +231,9 @@ struct dsi_panel {
 
 	struct brightness_alpha_pair *fod_dim_lut;
 	uint32_t fod_dim_lut_count;
+#ifdef CONFIG_DRM_SDE_EXPO
+	bool dimlayer_exposure;
+#endif
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
--- techpack/display/msm/sde/sde_crtc.c
+++ techpack/display/msm/sde/sde_crtc.c
@@ -4792,6 +4792,13 @@ static int sde_crtc_exposure_atomic_check(struct sde_crtc_state *cstate,
 		struct plane_state *pstates, int cnt)
 {
 	int i, zpos = 0;
+	struct dsi_display *dsi_display = get_main_display();
+	struct dsi_panel *panel = dsi_display->panel;
+
+	if (!panel->dimlayer_exposure || panel->doze_enabled) {
+		cstate->exposure_dim_layer = NULL;
+		return 0;
+	}
 
 	for (i = 0; i < cnt; i++) {
 		if (pstates[i].stage > zpos)
