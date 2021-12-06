2db3593c78c318bf78aa68e3a361bc243f924fb2
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2021-12-05 22:02:53 -0600

    Revert "kernel: stune_assist: tune for kona"
    This reverts commit ce65fb12e2a96948544155ee78c012440669139f.

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -897,9 +897,9 @@ static void write_default_values(struct cgroup_subsys_state *css)
 	static struct st_data st_targets[] = {
 		{ "audio-app",	0, 0, 0, 0 },
 		{ "background",	0, 0, 0, 0 },
-		{ "foreground",	0, 0, 0, 0 },
+		{ "foreground",	0, 1, 0, 1 },
 		{ "rt",		0, 0, 0, 0 },
-		{ "top-app",	1, 1, 1, 1 },
+		{ "top-app",	1, 1, 0, 1 },
 	};
 	int i;
 
