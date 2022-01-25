48cc259679a3df5aca708a839ef34b382d111ec6
Ramii Ahmed <ramy@ahmedramy.com>
2022-01-25 04:39:39 +0200

    drivers: gpio-testing-mode: remove unused functions
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- drivers/misc/gpio-testing-mode.c
+++ drivers/misc/gpio-testing-mode.c
@@ -27,35 +27,6 @@ struct testing_mode_data {
 	struct delayed_work debounce_work;
 };
 
-static ssize_t gpio_status_show(struct device *dev,
-		struct device_attribute *attr, char *buf)
-{
-	struct testing_mode_data *data = dev_get_drvdata(dev);
-
-	return scnprintf(buf, PAGE_SIZE, "%d\n", gpio_get_value(data->status_gpio));
-}
-static DEVICE_ATTR(status, S_IRUGO, gpio_status_show, NULL);
-
-
-static void gpio_debounce_work(struct work_struct *work)
-{
-	return;
-}
-static irqreturn_t testing_threaded_irq_handler(int irq, void *irq_data)
-{
-	return IRQ_HANDLED;
-}
-
-static int testing_mode_probe(struct platform_device *pdev)
-{
-	return 0;
-}
-
-static int testing_mode_remove(struct platform_device *pdev)
-{
-	return 0;
-}
-
 MODULE_AUTHOR("Tao Jun<taojun@xiaomi.com>");
 MODULE_DESCRIPTION("A dummy driver for GPIO testing mode");
 MODULE_LICENSE("GPL");
