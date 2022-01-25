c158ddeab44d4a551127090cd938d7297313cee9
Ramii Ahmed <ramy@ahmedramy.com>
2022-01-25 02:40:08 +0200

    sm8250: drivers: dma-buf: fixup-remove undefined function
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- drivers/dma-buf/dma-buf.c
+++ drivers/dma-buf/dma-buf.c
@@ -171,8 +171,6 @@ static int dma_buf_release(struct inode *inode, struct file *file)
 		pr_warn_ratelimited("Leaking dmabuf %s because destructor failed error:%d\n",
 				    dmabuf->buf_name, dtor_ret);
 
-	trace_dma_buf_release(inode, file, tgid);
-
 	dma_buf_ref_destroy(dmabuf);
 
 	if (dmabuf->resv == (struct reservation_object *)&dmabuf[1])
