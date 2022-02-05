17a2deae10ccdd84a58227b7d2e6fa077d487d7d
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2022-02-05 10:50:44 +0530

    techpack: display: remove exposure adjustment driver
    This reverts commit 494155b6a17cbe45b905ffd2c9a39c5b1488fc9c.
    
    This reverts commit 4c5942d588668cbc0bb0b3498d0e79eb638d6635.
    
    This reverts commit acb2b3f75a1496b3d11eeb666f3597de8896a8be.
    
    This reverts commit 81d01616df3afe45d850bf8e2ab9b979b31b3c0d.

--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -242,8 +242,6 @@ struct dsi_panel {
 
 	bool doze_enabled;
 	enum dsi_doze_mode_type doze_mode;
-	bool resend_ea;
-	bool resend_ea_hbm;
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
