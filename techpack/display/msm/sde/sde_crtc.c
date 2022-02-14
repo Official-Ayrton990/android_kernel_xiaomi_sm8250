b05c89796e51b88ad1b54f3a88ebcea4cb0293a8
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2022-02-14 07:52:12 +0530

    Revert "techpack: display: disable exposure dimlayer driver in doze_mode"
    This reverts commit 8491d72e8b03371baf6d07dc0dc7a10e0ddc147e.

--- techpack/display/msm/sde/sde_crtc.c
+++ techpack/display/msm/sde/sde_crtc.c
@@ -2594,10 +2594,6 @@ static int sde_crtc_config_exposure_dim_layer(struct drm_crtc_state *crtc_state,
 		return -EINVAL;
 	}
 
-	if (!alpha || panel->doze_enabled) {
-		cstate->exposure_dim_layer = NULL;
-		return 0;
-	}
 
 	if ((stage + SDE_STAGE_0) >= kms->catalog->mixer[0].sblk->maxblendstages) {
 		return -EINVAL;
@@ -4795,7 +4791,7 @@ static int sde_crtc_exposure_atomic_check(struct sde_crtc_state *cstate,
 	struct dsi_display *dsi_display = get_main_display();
 	struct dsi_panel *panel = dsi_display->panel;
 
-	if (!panel->dimlayer_exposure || panel->doze_enabled) {
+	if (!panel->dimlayer_exposure) {
 		cstate->exposure_dim_layer = NULL;
 		return 0;
 	}
