35bb0a4ce11e15dfe18216bf9daecff84217cea7
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2022-02-17 09:48:18 +0530

    techpack: display: add a correction to dimlayer interpolation
    Our dimlayer is calculated using a linear model, regardless a delay of 10 was added to the panel on both to avoid brightness flashing (panel is slow to react), on FOD-HBM ON/OFF. This causes the model to be unaccurate to generate the alpha value for dimlayer. Add .01 as equivalent correction factor for this delay with a +- .0025 error. This fixes the black lut at FOD Pressed/Unpressed.
    
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -798,7 +798,9 @@ static u32 dsi_panel_get_backlight(struct dsi_panel *panel)
 
 static uint32_t interpolate(uint32_t x, uint32_t xa, uint32_t xb, uint32_t ya, uint32_t yb)
 {
-	return ya - (ya - yb) * (x - xa) / (xb - xa);
+	u32 base = ya - (ya - yb) * (x - xa) / (xb - xa);
+
+	return base - 0.0125*base;
 }
 
 
